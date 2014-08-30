a7ec87028fa45ab090a2bc0829e860ea8501734d
Marcelo Fernandes <mmelo_fernandes@hotmail.com>
2014-08-30 09:54:08 -0300

    Task 4.12 finished for OO

--- src/main/java/br/ufpb/dce/aps/coffeemachine/impl/SweetCream.java
+++ src/main/java/br/ufpb/dce/aps/coffeemachine/impl/SweetCream.java
@@ -2,6 +2,7 @@ package br.ufpb.dce.aps.coffeemachine.impl;
 
 import br.ufpb.dce.aps.coffeemachine.ComponentsFactory;
 import br.ufpb.dce.aps.coffeemachine.Dispenser;
+import br.ufpb.dce.aps.coffeemachine.Messages;
 
 public class SweetCream extends Cafe{
 
@@ -11,6 +12,10 @@ public class SweetCream extends Cafe{
 	
 	protected boolean contemIngredientes() {
 		factory.getCupDispenser().contains(1);
+		if(receita.isSteamed()){
+			dispensers.get("Milk").contains(150.0);
+			return true;
+		}
 		if(!dispensers.isEmpty()){
 			dispensers.get("Milk").contains(120.0);
 			dispensers.get("Chocolate Powder").contains(20.0);
@@ -26,6 +31,11 @@ public class SweetCream extends Cafe{
 	@Override
 	protected void liberarPo(){
 		if(!dispensers.isEmpty()){
+			if(receita.isSteamed()){
+				dispensers.get("Milk").release(150.0);
+				factory.getSteamer().steam();
+				return;
+			}
 			dispensers.get("Milk").release(120.0);
 			dispensers.get("Chocolate Powder").release(20.0);
 			factory.getSugarDispenser().release(5.0);	
